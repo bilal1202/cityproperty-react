e1634030bfebe0dbe9b4c4558382377f24d304b6
bilal1202 <cadet.bilal.87@gmail.com>
2023-04-28 17:38:38 +0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
- 
+import { BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignLn from "./pages/SignLn";
+
+
 
 function App() {
   return (
     <>
 
-     <h1>Hello world</h1>
+<Router>
+
+<Routes>
+
+  <Route path="/" element={<Home/>}></Route>
+  <Route path="/profile" element={<Profile/>}></Route>
+  <Route path="/sign-in" element={<SignLn/>}></Route>
+  <Route path="/sign-up" element={<SignUp/>}></Route>
+  <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+  <Route path="/offers" element={<Offers/>}></Route>
+
+</Routes>
+
+</Router>
 
     </>
   );
---
+++ src/pages/SignLn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignLn() {
+  return (
+    <div>
+      Sign In
+    </div>
+  )
+}
