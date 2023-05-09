39a033229e205582743949c05cb702a5bb9b546b
bilal1202 <cadet.bilal.87@gmail.com>
2023-05-10 03:49:25 +0500

    complete the signin functionality

--- src/pages/SignLn.jsx
+++ src/pages/SignLn.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import {AiFillEyeInvisible,AiFillEye} from "react-icons/ai"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword ,  getAuth } from 'firebase/auth';
+import {toast} from "react-toastify"
+
+
 
 export default function SignLn() {
   const[showPassword,setShowPassword]=useState(false);
@@ -11,6 +15,7 @@ export default function SignLn() {
   });
 
   const {email,password}=formData;
+  const navigate = useNavigate()
 
   function onChange(e){
 setFormData((prevState)=>({
@@ -18,6 +23,19 @@ setFormData((prevState)=>({
   [e.target.id] : e.target.value,
 }))
   }
+
+async function onSubmit(e){
+  e.preventDefault()
+  try {
+    const auth=getAuth()
+    const userCrediental=await signInWithEmailAndPassword(auth,email,password)
+    if(userCrediental.user){
+     navigate("/")
+    }
+  } catch (error) {
+    toast.error("Bad user credentials")
+  }
+}
 
   return (
     <section>
@@ -35,7 +53,7 @@ setFormData((prevState)=>({
           
           </div>
           <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-            <form>
+            <form onSubmit={onSubmit}>
           <input className=' mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'  type="email"  id='email' value={email} onChange={onChange}  placeholder='Email'/>
             
             <div className='relative mb-6`'>
